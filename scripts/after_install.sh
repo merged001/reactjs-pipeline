0ade3742102b0df52e7ae5bbef0fde6c0e113726
Jatin Gupta <201951073@iiitvadodara.ac.in>
2022-10-12 16:49:57 +0530

    dddd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/server
-npm install
-npm install --save react react-dom react-scripts react-particles-js
-npm install pm2 -g
+echo "No Need of Installing dependancy anything"
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "covidapp" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+echo "ngnix will automatically start pointing to this new build"
